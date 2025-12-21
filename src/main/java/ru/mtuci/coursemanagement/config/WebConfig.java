dc787b3b17e503c7e3d9f9534db29998174c1145
warasau <galaksylive@gmail.com>
2025-12-21 18:25:27 +0300

    Add GitHub CI pipeline configuration with secret scanner (gitleaks)

--- src/main/java/ru/mtuci/coursemanagement/config/WebConfig.java
+++ src/main/java/ru/mtuci/coursemanagement/config/WebConfig.java
@@ -14,4 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*");
 
     }
+    String password = "temp";
+    String API_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 }
